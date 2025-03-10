1494211b9cc390825dbb099633d79160a7d14a6d
francisohara24 <franciskohara@gmail.com>
2025-03-10 00:07:02 -0400

    test: add test input file for task 4

---
+++ src/project_2/task_4/clite_parser_test.c
@@ -0,0 +1,12 @@
+int main() {
+  // this is a single-line comment
+  /* this is sa multiline comment/ containing backslash*/
+
+  int a = 6;
+  int b = 5.0;
+
+  if( a < b ) {
+    a = a + b;
+  }
+
+}
