07fddcded0fb195d1f419c76864cf83b6206ed17
francisohara24 <franciskohara@gmail.com>
2025-03-18 09:49:20 -0400

    test: update multi-line comment test

--- src/project_2/task_4/clite_parser_test.c
+++ src/project_2/task_4/clite_parser_test.c
@@ -1,6 +1,8 @@
 int main() {
   // this is a single-line comment
-  /* this is sa multiline comment/ containing backslash*/
+  /* this is a multiline comment
+
+containing a /backslash/ */
 
   int a = 6;
   int b = 5.0;
