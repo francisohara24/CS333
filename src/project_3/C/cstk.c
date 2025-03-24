7704ede6727d8cb64a37b4a1b9d631e6a8b98316
francisohara24 <franciskohara@gmail.com>
2025-03-23 22:43:04 -0400

    feat: add implementation for all methods of stack data structure

--- src/project_3/C/cstk.c
+++ src/project_3/C/cstk.c
@@ -30,11 +30,67 @@ int stk_full(Stack *stack) {
 
 void stk_push(Stack *stack, int value) {
     if (!stk_full(stack)) {
-        *stack->top = value;
+        *(stack->top) = value;
         stack->top++;
-    }
-    else
-        printf("Warning: attempting to push value on a full stack.");
+    } else
+        printf("Warning: attempting to push value on a full stack.\n");
 }
 
 
+int stk_peek(Stack *stack) {
+    if (!stk_empty(stack)) {
+        return *(stack->top);
+    }
+    printf("Warning: attempting to peek an empty stack.\n");
+    return 0;
+}
+
+
+int stk_pop(Stack *stack) {
+    if (!stk_empty(stack)) {
+        int value = *(stack->top);
+        stack->top--;
+        return value;
+    }
+    printf("Warning: attempting to pop an empty stack.\n");
+    return 0;
+}
+
+
+void stk_display(Stack *stack, int order) {
+    if (order == 1) {
+        int *ptr = stack->data;
+        while (ptr != stack->top) {
+            printf("%d ", *ptr);
+            ptr++;
+        }
+    } else {
+        int *ptr = stack->top - 1;
+        while (ptr != stack->data - 1) {
+            printf("%d ", *ptr);
+        }
+    }
+    printf("\n");
+}
+
+
+void stk_destroy(Stack *stack) {
+    free(stack->data);
+    free(stack->top);
+    free(stack);
+}
+
+
+Stack *stk_copy(Stack *stack) {
+    Stack *new_stack = (Stack *) malloc(sizeof(Stack));
+    new_stack->data = (int *) malloc(stack->capacity * sizeof(int));
+    new_stack->capacity = stack->capacity;
+    new_stack->top = new_stack->data;
+    int *ptr = stack->data;
+    while (ptr != stack->top) {
+        *new_stack->top = *ptr;
+        new_stack->top++;
+        ptr++;
+    }
+    return new_stack;
+}
