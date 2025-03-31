ad8f1fa78b78641a51312596fc8788bc4074de25
francisohara24 <franciskohara@gmail.com>
2025-03-30 23:24:06 -0400

    feat: add initial code to demonstrate variable declaration and scoping rules in JavaScript

--- src/project_3/JS/task_1/variableDeclarationAndScope.js
+++ src/project_3/JS/task_1/variableDeclarationAndScope.js
@@ -1,1 +1,21 @@
+/** Variables may be declared using either the `var`, `let`, or `const` keyword. */
+var foo;
+let bar;
+const baz = 42;
+
+/**
+ * Rule 1: Variables declared using the `var` keyword are function-scoped or globally scoped
+ */
+var foo = 42;
+
+function func1(){
+    var bar = 45;
+    console.log(bar);
+}
+
+function func2(){
+
+}
+
+
 // const variable without initializer is not allowed.
